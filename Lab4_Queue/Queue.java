d3f1c9ba4b705b57ee812e7fffc54d3232625a25
Akshat Singh <144692981+Akshatcodesyou@users.noreply.github.com>
2025-03-05 10:42:25 +0400

    4th

--- Lab4_Queue/Queue.java
+++ Lab4_Queue/Queue.java
@@ -16,7 +16,7 @@ public class Queue {
 
     public void enqueue(int num){
         if(rear == Size-1){
-            System.out.println("Queue full!")
+            System.out.println("Queue full!");
         }
         else{
             if(front == -1){
@@ -24,17 +24,33 @@ public class Queue {
             }
             rear++;
             arr[rear] = num;
-            System.out.println("Number queued");
+            System.out.println("Number queued" + " Num: " + num);
+        }
+    }
+
+    public void dequeueall(){
+        for(int i = this.front; i <= this.rear; i++){
+            System.out.println("Dequeuing " + (i+1) + " element: " + arr[i]);
+        }
+    }
+    public void dequeue(){
+        if(front == -1){
+            System.out.println("Queue is empty! ");
+        }
+        else{
+            System.out.println("")
         }
     }
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Queue q1 = new Queue(5);
-        q1.enqueue(5);
-        q1.enqueue(4);
-        q1.enqueue(3);
+        q1.enqueue(1);
         q1.enqueue(2);
+        q1.enqueue(3);
+        q1.enqueue(4);
+        q1.enqueue(5);
+        q1.dequeue();
         scan.close();
     }
 }
